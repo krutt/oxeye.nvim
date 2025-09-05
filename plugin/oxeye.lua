86efd9235826c9a6564a69d153f9a1d3324f94c6
Sitt Guruvanich <aekazitt@gmail.com>
2025-09-05 20:44:30 +0700

    feat: validate libraries needed before launch

--- plugin/oxeye.lua
+++ plugin/oxeye.lua
@@ -1,5 +1,9 @@
 -- plugin/argute.lua --
 
-if not pcall(require, 'nvim-treesitter.configs') then
+
+if not pcall(require, 'lspconfig')
+  or not pcall(require, 'nvim-treesitter.configs')
+  or not pcall(require, 'plenary')
+then
   return
 end
